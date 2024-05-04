c007347be03fd67ce683a4287cf35c22a2e0e3ba
husongtao <15309487104@163.com>
2024-05-04 17:04:58 +0800

    close server

--- vite.config.js
+++ vite.config.js
@@ -13,15 +13,15 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  server:{
-    proxy:{
-       '/api':{
-           target:"http://101.43.77.156:8080", //跨域地址
-           changeOrigin:true, //支持跨域
-           rewrite:(path) => path.replace(/^\/api/, "")//重写路径,替换/api
-       }
-    }
- },
+//   server:{
+//     proxy:{
+//        '/api':{
+//            target:"http://101.43.77.156:8080", //跨域地址
+//            changeOrigin:true, //支持跨域
+//            rewrite:(path) => path.replace(/^\/api/, "")//重写路径,替换/api
+//        }
+//     }
+//  },
   optimizeDeps: {
     include: [
       `${prefix}/language/json/json.worker`,
